618795bf50d0247ed8248214a13c5c98f91b1707
SEBASTIAN WIDECKI <>
2022-11-10 17:23:06 +0000

    added button functionality. removed herobrine.

--- cities.js
+++ cities.js
@@ -1,3 +1,8 @@
+function preload() {
+  song = loadImage('fb8.jpg');
+}
+var scene = 0;
+var city = null;
 function setup() {
   createCanvas(400, 400);
 }
@@ -6,10 +11,70 @@ var cities = ["Ottawa", "New York", "London"];
 var buttonX = 100;
 var buttonY = 125;
 
+ var btn1 = new Button({
+    x: buttonX,
+    y: buttonY,
+    colour: [11, 252, 3],
+    label: cities[0],
+    onClick: function() {
+      print("ottawa");
+      city = cities[0];
+              scene = 1;
+
+    }
+});
+var btn2 = new Button({
+    x: buttonX,
+    y: buttonY+75,
+      colour: [11, 252, 3],
+    label: cities[1],
+    onClick: function() {
+      print("new york");
+            city = cities[1];
+              scene = 1;
+
+    }
+});
+var btn3 = new Button({
+    x: buttonX,
+    y: buttonY+150,
+      colour: [11, 252, 3],
+
+    label: cities[2],
+    onClick: function() {
+      print("london");
+            city = cities[2];
+        scene = 1;
+
+    }
+});
+var btn4 = new Button({
+    x: buttonX,
+    y: buttonY+225,
+      colour: [255, 0, 0],
+
+    label: 'ohio',
+    onClick: function() {
+      print("only in ohio");
+              scene = -1;
+    }
+});
+
 function draw() {
   background(220);
-  fill(0, 0, 0);
   text("Select a City for Simulation", buttonX+25, buttonY-25);
+  if (scene === 0) {
+  btn1.draw();
+  btn2.draw();
+  btn3.draw();
+  btn4.draw();
+  }
+  if (scene === -1) {
+    song.resize(500, 0)
+    image(song, 0, 0)
+  }
+  /*
+  fill(0, 0, 0);
   fill(12, 203, 102);
   for(var i =0; i <cities.length; i++) {
     fill(12, 203, 102);
@@ -20,8 +85,9 @@ function draw() {
 
   mouseClicked = function() {
     if (mouseX >= buttonX && mouseX <= buttonX+200 && mouseY >= buttonY && mouseY <= buttonY+50) {
-      
+      print("ottawa");
     } 
   }
+  */
 }
 
